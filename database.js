2e14a617cfd7e2873bb17f4e5948512268191c14
smska2022 <2009sms@rambler.ru>
2022-04-22 09:52:06 +0300

    ДЗ 8

---
+++ database.js
@@ -0,0 +1,42 @@
+const db = {
+  posts: [
+    {
+      title: 'Горный пейзаж',
+      image: 'https://natworld.info/wp-content/uploads/2018/01/%D0%A1%D0%BE%D1%87%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BD%D0%B0-%D1%82%D0%B5%D0%BC%D1%83-%D0%9F%D1%80%D0%B8%D1%80%D0%BE%D0%B4%D0%B0.jpeg',
+      text: 'Горный пейзаж',
+      tag: 'пейзаж'
+    },
+    {
+      title: 'Озеро в горах',
+      image: 'https://n1s2.hsmedia.ru/60/b5/cc/60b5cc5266a98b966e2f35c57ed388c8/690x380_0x0a330c2a_12567029551616070388.jpeg',
+      text: 'Озеро',
+      tag: 'пейзаж'
+    },
+    {
+      title: 'Водопад',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/Hopetoun_falls.jpg/220px-Hopetoun_falls.jpg',
+      text: 'Водопад в горах',
+      tag: 'пейзаж'
+    },
+    {
+      title: 'Река в лесу',
+      image: 'https://images11.popmeh.ru/upload/img_cache/134/1341c77acc82b4450dac6d54f5e5a038_ce_1920x1024x0x128_cropped_666x444.jpg',
+      text: 'Осенний пейзаж',
+      tag: 'пейзаж'
+    },
+    {
+      title: 'Самые высокие горы',
+      image: 'https://moya-planeta.ru/upload/images/xl/95/fe/95fe44d0e5fe53e49d874f9c2e07381ca8ea823a.jpg',
+      text: 'Тут описание самых высоких гор и где находятся',
+      tag: 'Горы'
+    }
+ 
+  ],
+}
+const users = {
+  user: [
+  ],
+}
+module.exports = {
+  db, users,
+}
---
+++ life.js
@@ -0,0 +1,137 @@
+
+const path = require('path');
+const express = require('express');
+const sessions = require('express-session');
+//const bodyParser = require('body-parser');
+const hbs = require('hbs');
+const bcrypt = require('bcrypt');
+const { db, users } = require('./database');
+const { checkAuth } = require('./middleware/checkAuth');
+const secretKey = 'sdfk345kjgkk';
+
+const app = express();
+
+app.set('view engine', 'hbs');
+app.set('cookieName', 'sid');
+app.set('views', path.join(process.env.PWD, './handlebars'));
+hbs.registerPartials(path.join(process.env.PWD, './handlebars'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname)));
+app.use(express.json()); // для распознавания входящего объекта запроса как объекта JSON
+
+
+
+
+app.use(sessions({
+    name: app.get('cookieName'),
+    secret: secretKey,
+    resave: false, 
+    saveUninitialized: false, 
+    cookie: { 
+      httpOnly: true, 
+      maxAge: 86400 * 1e3, 
+    },
+  }));
+
+
+app.use((req, res, next) => {
+    const usersEmail = req.session?.user?.email;
+    if (usersEmail) {
+      const currentUser = users.user.find((user) => user.email === usersEmail);
+      res.locals.name = currentUser.name
+     
+    }
+    next()
+  });
+
+
+
+// регистрация
+app.get('/', (req, res) => {
+    res.render('header')
+  });
+
+
+app.get('/signup', async (req, res) => {
+    res.render('signup');
+  });
+
+
+app.post('/signup', async (req, res) => {
+    const { name, email, password } = req.body
+    const hashPass = await bcrypt.hash(password, 5)
+    users.user.push({
+      name,
+      email,
+      password: hashPass,
+    })
+    req.session.user = {
+      email,
+    }
+    res.redirect('/signin')
+  })
+
+
+
+  // вход в систему
+app.get('/signin', async (req, res) => {
+    res.render('signin')
+  });
+
+app.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    const currentUser = users.user.find((user) => user.email === email)
+    if (currentUser) {
+      if (await bcrypt.compare(password, currentUser.password)) {
+        req.session.user = { email };
+        return res.redirect('/upload');
+      }
+    }
+    return res.redirect('/signin');
+});
+
+// выход из сессии
+app.get('/signout', (req, res) => {
+    req.session.destroy((err) => {
+      if (err) return res.redirect('/')
+  
+      res.clearCookie(req.app.get('cookieName'))
+  
+      return res.redirect('/')
+    });
+});
+
+// отрисовка постов
+app.get('/upload', checkAuth, (req, res) => {
+  const postQuery = req.query
+  let postsForRender = db.posts
+  if (postQuery.limit !== undefined && Number.isNaN(+postQuery.limit) === false) {
+    postsForRender = db.posts.slice(0, postQuery.limit)
+  }
+  if (postQuery.reverse !== false) {
+    postsForRender = db.posts.reverse()
+  }
+  console.log(postsForRender);
+  res.render('main', {list : postsForRender} )
+});
+   
+ 
+
+//  контроллер добавления поста
+app.post('/addPost', (req, res) => {
+    db.posts.push({
+        "title": req.body.title,
+        "image": req.body.image,
+        "text": req.body.text,
+        "tag": req.body.tag,
+        "email": req.session.user.email
+    });
+    //console.log(db.posts);
+    res.redirect(req.get('referer'));
+});
+
+app.listen(3000, () => {
+    console.log('Application listening on port 3000!')
+    console.log('http://127.0.0.1:3000')
+});
+
